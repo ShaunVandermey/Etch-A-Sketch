306c4063f6aafe85ff3c18b1795ee08d9c0f865a
Shaun Vandermey <shaunyv132@outlook.com>
2023-05-10 22:36:20 +1000

    Improve layout, styles, and script. Add a reset button.

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 const mainBox = document.querySelector(".mainDrawBoxes");
 console.log(mainBox);
 let boxes = [];
-let initialBoxNum = 16;
+let initialBoxNum = 100;
+let isMouseDown = false;
+mainBox.onmouseup = dontColour();
 //function that creates a square subdiv with all attached info
 //such as on mouse over = change colour
 function addBox(){
 
     let newBox = document.createElement("div");
     mainBox.classList.add("content");
-    newBox.addEventListener("mouseenter", changeToBlack);
-    newBox.addEventListener("mouseleave", changeToWhite);
+    newBox.addEventListener("mouseenter", addHover);
+    newBox.addEventListener("mouseleave", removeHover);
+    newBox.onmousedown = recolour;
+    newBox.onmouseup = dontColour;
+    //newBox.addEventListener("onclick", recolour);
     newBox.classList.add("simpleBorder");
     boxes.push(newBox);
     mainBox.appendChild(newBox);
 }
 
-function changeToBlack(e){
+function addHover(e){
     //change colour of given box to black
+    this.classList.add("hovered");
+    if(isMouseDown == true){
+        this.classList.add("black");
+    }
+}
+
+function removeHover(e){
+    this.classList.remove("hovered");
+}
+
+function recolour(e){
     this.classList.add("black");
+    isMouseDown = true;
 }
 
-function changeToWhite(e){
-    this.classList.remove("black");
+function dontColour(e){
+    isMouseDown = false;
 }
 
-
+function resetBoxes(){
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].className = "";
+        boxes[i].classList.add("simpleBorder");
+    }
+}
 
 
 for(i = 0; i < initialBoxNum; i++){
