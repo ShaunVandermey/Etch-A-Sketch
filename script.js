424308488967b0e1cb2924a1668f358a7e034697
Shaun Vandermey <shaunyv132@outlook.com>
2023-06-02 01:12:57 +1000

    Improve final look and feel of the site and clean up unneeded text.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let boxes = [];
 
 let horizontalBoxNum = 5;
 let verticalBoxNum = 5;
-let boxUpperLimit = 30;
+let boxUpperLimit = 35;
 let initialBoxNum = verticalBoxNum * horizontalBoxNum;
 let isMouseDown = false;
 let currentColour = "#000000";
@@ -13,24 +13,18 @@ let currentColour = "#000000";
 document.documentElement.style.setProperty(`--horLength`, horizontalBoxNum);
 document.documentElement.style.setProperty(`--vertLength`, verticalBoxNum);
 
-//check to see if the mouse is down on every move because the normal
-//mouseup and mousedown events down't always fire, especially if there's
-//gaps between objects
-/*
-window.addEventListener("mousemove", checkMouse);
-function checkMouse(e){
-    console.log(e.buttons);
-    if(e.buttons == 0){
-        isMouseDown = false;
-    }
-    if(e.buttons == 1){
-        isMouseDown = true;
-    }
-}
-*/
-
 document.body.onmousedown = () => (isMouseDown = true);
 document.body.onmouseup = () => (isMouseDown = false);
+
+window.onload = main();
+
+function main(){
+    document.getElementById("horizontal").value = horizontalBoxNum;
+    document.getElementById("vertical").value = verticalBoxNum;
+    for(i = 0; i < initialBoxNum; i++){
+        addBox();
+    }
+}
 
 //function that creates a square subdiv with all attached info
 //such as on mouse over = change colour
@@ -40,7 +34,6 @@ function addBox(){
     mainBox.classList.add("content");
     newBox.addEventListener("mouseenter", addHover);
     newBox.addEventListener("mouseleave", removeHover);
-    //newBox.onmousedown = recolour;
     newBox.addEventListener("mouseover", recolour);
     newBox.addEventListener("mousedown", recolour);
     newBox.addEventListener("ondragstart", dragStart);
@@ -59,25 +52,20 @@ function eraserMode(){
     colourPicker.value = "#FFFFFF";
 }
 
+//hover effect for better identification of current div
 function addHover(e){
-    //change colour of given box to black
     this.classList.add("hovered");
-    /*
-    if(isMouseDown == true){
-        this.classList.add("black");
-    }
-    */
 }
 
 function removeHover(e){
     this.classList.remove("hovered");
 }
 
+
 function recolour(e){
     if(e.type === "mouseover" && !isMouseDown){   
         return;
     }
-    //this.classList.add("black");
     this.style.backgroundColor = currentColour;
 }
 
@@ -89,6 +77,7 @@ function resetBoxes(){
     }
 }
 
+//prevent some weird drag behaviours
 function dragStart(e){
     e.preventDefault();
 }
@@ -123,10 +112,3 @@ function createNewBoxes(){
     }
 }
 
-
-for(i = 0; i < initialBoxNum; i++){
-    addBox();
-}
-
-//add a button to top of screen that sends a promt asking for new dimensions of grid
-//when user enters a number (max 100), delete current grid and make new one (code should be reusuable)
